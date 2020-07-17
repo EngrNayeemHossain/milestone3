0aadbf022fbdccd9c832a09984ab4ec9a65272a1
Ayesha Siddika <ayesha56sid@gmail.com>
2020-07-17 08:22:19 +0600

    assignment done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+//how to convert feet to mile
+
 function feetToMile(feet){
     var mile=feet*0.000189394;
     return mile;
@@ -5,3 +7,27 @@ function feetToMile(feet){
 
 var mile=feetToMile(15);
 console.log(mile);
+
+//how to calculate total amount of wood
+
+function woodCalculator(chair,table,bed){
+    var wood=chair*1+table*3+bed*5;
+    return wood;
+}
+var wood=woodCalculator(5,7,8);
+console.log(wood);
+
+function brickCalculator(){
+    if(0<a<=10){
+        a=a*15000;
+    }
+    else if(10<a<20){
+        a=a*12000;
+    }
+    else(a>21){
+        a=a*1000;
+    }
+    return a;
+}
+console.log(a);
+
